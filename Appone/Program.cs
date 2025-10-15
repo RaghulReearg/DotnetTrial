9da6a118b90c54858eba9bf819800c0f6308d9b9
Raghul <Raghul@novigosolutions.com>
2025-10-16 01:31:17 +0530

    test

---
+++ Appone/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Appone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            Console.ReadKey();
+        }
+      
+        
+    }
+}
---
+++ WebApplication2/CustomAttributes/ApiAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Class| AttributeTargets.Method)]
+    public class ApiAttribute:Attribute
+
+    {
+        public ApiAttribute()
+        {
+            var keyvalue = "Test values";
+        }
+    }
+}
